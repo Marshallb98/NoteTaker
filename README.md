a628a2b1adb5f51a8765ccb9ccea0e3b57915804
Marshall Bertschy <basemarsh@gmail.com>
2021-01-12 23:42:17 -0600

    added final touches

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # NoteTaker
+
+
+### Description
+A application to help take notes and keep track of them! 
+
+### Table of Contents
+
+* [Installation](#installation)
+* [Hosted Site](#hosted)
+* [License](#license)
+* [Questions](#questions)
+
+### Installation
+To install necessary dependencies, run the following command:
+```
+npm i
+```
+The program can then be run using the following command line in the bash terminal or integrated terminal:
+```
+node server.js
+```
+### Hosted
+The app is hosted using Heroku here at this link: [Heroku Link](https://mysterious-eyrie-18588.herokuapp.com/)
+
+
+### Questions
+If you have any questions feel free to reach out to my on my github account!
--- server.js
+++ server.js
@@ -1,36 +1,46 @@
-const express = require("express")
-const path = require("path")
-var fs = require("fs")
-const PORT = process.env.PORT || 3030
-const notesDB = JSON.parse(fs.readFileSync("./db/db.json", "utf8"))
-const app = express()
+var express = require("express");
+var path = require("path");
+const fs = require("fs");
+var PORT = process.env.PORT || 3030;
+var app = express();
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+const notesDB = './db/db.json';
 
 app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"))
-})
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
 
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
-})
+});
 
 app.get("/api/notes", function (req, res) {
-    return res.json(notesDB)
-})
+    res.sendFile(path.join(__dirname, "/db/db.json"))
+});
 
-app.post("/api/notes", function(req, res){
+app.post("/api/notes", function (req, res) {
     let postNote = req.body
+    postNote.id = Math.floor(Math.random() * 10000)
     notesDB.push(postNote)
     return res.json(notesDB)
-})
-
-app.delete("/api/notes/:id", function (req,res){
-
-})
-
+});
+app.delete("/api/notes/:id", function (req, res) {
+    console.log(notesDB)
+    var noteToRemove = req.params.id
+    notesDB = notesDB.filter(function (data) {
+        return data.id != noteToRemove
+    });
+    let updatedNotes = JSON.stringify(notesDB)
+    fs.writeFile("./db/db.json", updatedNotes, err => {
+        if (err) throw err
+        else {
+            return res.json(updatedNotes)
+        }
+    });
+});
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT)
-})
+});
