779165e2bdc0b35a532ba5dce2d3d1040f332f4c
Marshall Bertschy <basemarsh@gmail.com>
2021-01-12 20:43:50 -0600

    added get req

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-var express = require("express");
-var path = require("path");
-var app = express();
-var PORT = process.env.PORT || 3030;
+const express = require("express")
+const path = require("path")
+var fs = require("fs")
+const PORT = process.env.PORT || 3030
+const notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"))
+const app = express()
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static("public"))
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+})
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+})
+
+app.get("/api/notes", function (req, res) {
+    return res.json(notes)
+})
 
 app.listen(PORT, function () {
-    console.log("App listening on PORT: " + PORT);
-});
+    console.log("App listening on PORT: " + PORT)
+})
